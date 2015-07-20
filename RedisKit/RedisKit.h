f3ff51ba9892563ccb6bbe69ca46e8009dc9630a
Dmitry Bakhvalov <dizzus@gmail.com>
2015-07-20 19:23:01 +0300

    Initial Commit

---
+++ RedisKit/RedisKit.h
@@ -0,0 +1,19 @@
+//
+//  RedisKit.h
+//  RedisKit
+//
+//  Created by Дмитрий Бахвалов on 20.07.15.
+//  Copyright (c) 2015 Dmitry Bakhvalov. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for RedisKit.
+FOUNDATION_EXPORT double RedisKitVersionNumber;
+
+//! Project version string for RedisKit.
+FOUNDATION_EXPORT const unsigned char RedisKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RedisKit/PublicHeader.h>
+
+
